6f6f9942408470eaa8cdc5e94878ff90d7a16a17
RTm-Working-Tools <chin123kr@gmail.com>
2025-08-07 20:27:42 +0900

    Disabling HTMLPROOFER

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754563559',resources: [
+    cacheName: 'chirpy-1754566016',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
