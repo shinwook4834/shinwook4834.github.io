bfd929765d48cef442f8fc3fbde64c15a33338d0
RTm-Working-Tools <chin123kr@gmail.com>
2025-08-07 22:31:01 +0900

    Final

--- _site/ko/assets/js/data/swconf.js
+++ _site/ko/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754566016',resources: [
+    cacheName: 'chirpy-1754573421',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
