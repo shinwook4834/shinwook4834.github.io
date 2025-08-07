03d4053c42d08e387956f199631528ce2507267b
RTm-Working-Tools <chin123kr@gmail.com>
2025-08-08 00:22:47 +0900

    Footer로

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754579015',resources: [
+    cacheName: 'chirpy-1754580114',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- _site/ko/assets/js/data/swconf.js
+++ _site/ko/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754579016',resources: [
+    cacheName: 'chirpy-1754580115',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
