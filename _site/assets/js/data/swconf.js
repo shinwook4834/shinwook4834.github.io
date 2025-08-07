3e3811cd3446a298b2c722945825bb5390a3547f
RTm-Working-Tools <chin123kr@gmail.com>
2025-08-08 00:04:36 +0900

    layout for languages

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754577415',resources: [
+    cacheName: 'chirpy-1754579015',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- _site/ko/assets/js/data/swconf.js
+++ _site/ko/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754577415',resources: [
+    cacheName: 'chirpy-1754579016',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
