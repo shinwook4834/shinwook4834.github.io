f0ed30e52a19eb592d2bdecb901170653babc048
RTm-Working-Tools <chin123kr@gmail.com>
2025-08-15 11:15:21 +0900

    Image loading improvement

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755184342',resources: [
+    cacheName: 'chirpy-1755223936',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
