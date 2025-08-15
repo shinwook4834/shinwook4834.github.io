e97dfcda55746548c7c1ab616fcaa82f3712e6dd
RTm-Working-Tools <chin123kr@gmail.com>
2025-08-15 16:18:44 +0900

    avatar change

--- _site/ko/assets/js/data/swconf.js
+++ _site/ko/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755223936',resources: [
+    cacheName: 'chirpy-1755240885',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
