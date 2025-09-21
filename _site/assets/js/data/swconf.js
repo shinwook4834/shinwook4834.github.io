32968f9aee5c25dfa5c655f34bf009a3167d1d46
RTm-Working-Tools <chin123kr@gmail.com>
2025-09-21 16:22:08 +0900

    environment 설정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758439038',resources: [
+    cacheName: 'chirpy-1758439307',resources: [
       '/sermon/assets/css/jekyll-theme-chirpy.css',
       '/sermon/',
       
