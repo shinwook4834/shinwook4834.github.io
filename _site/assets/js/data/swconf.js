0933e494c38c059cfe9c1c9fdab91d038088796f
RTm-Working-Tools <chin123kr@gmail.com>
2025-10-17 23:17:02 +0900

    보완후 404에러

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760253066',resources: [
+    cacheName: 'chirpy-1760710576',resources: [
       '/sermon/assets/css/jekyll-theme-chirpy.css',
       '/sermon/',
       
