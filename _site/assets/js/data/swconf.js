8790edca7a5e50220778c13d2600e1c6851fcf2f
RTm-Working-Tools <chin123kr@gmail.com>
2025-10-26 20:34:10 +0900

    Test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761464770',resources: [
+    cacheName: 'chirpy-1761478432',resources: [
       '/sermon/assets/css/jekyll-theme-chirpy.css',
       '/sermon/',
       
