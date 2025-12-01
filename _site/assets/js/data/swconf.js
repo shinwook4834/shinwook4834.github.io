4ba6676906185e60264cc6659bf5dd36c8c225c5
RTm-Working-Tools <chin123kr@gmail.com>
2025-12-01 21:16:29 +0900

    언어전환

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764591184',resources: [
+    cacheName: 'chirpy-1764591305',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
