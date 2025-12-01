7cb3b50cf5211463a4f6d9e0d099654474300dfd
RTm-Working-Tools <chin123kr@gmail.com>
2025-12-01 20:49:44 +0900

    오류수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764589174',resources: [
+    cacheName: 'chirpy-1764589716',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
