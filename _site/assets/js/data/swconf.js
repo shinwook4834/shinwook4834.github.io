0b05380a9bcfd82eda37566b74c8fcb578a98d4c
RTm-Working-Tools <chin123kr@gmail.com>
2025-12-14 16:40:31 +0900

    수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765698000',resources: [
+    cacheName: 'chirpy-1765698020',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
